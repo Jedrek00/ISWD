34fb130004bc47c7596772df0f42d61a0e6d3c47
Jedrek00 <jedrek.koscielniak@gmail.com>
2023-05-29 21:45:40 +0200

    Add cross and super efficiency

--- Project4/dea.py
+++ Project4/dea.py
@@ -7,7 +7,7 @@ INPUT_DATA_PATH = "Project4/data/inputs.csv"
 OUTPUT_DATA_PATH = "Project4/data/outputs.csv"
 
 
-def calculate_efficiency(inputs, outputs, id=0) -> float:
+def solve_CCR(inputs, outputs, id, super_efficiency=False) -> LpProblem:
     num_examples = inputs.shape[0]
     num_inputs = inputs.shape[1]
     num_outputs = outputs.shape[1]
@@ -27,6 +27,9 @@ def calculate_efficiency(inputs, outputs, id=0) -> float:
     model += lpSum([inputs_weights[i] * inputs[id][i] for i in range(num_inputs)]) == 1
 
     for i in range(num_examples):
+        if super_efficiency:
+            if i == id:
+                continue
         model += lpSum(
             [outputs_weights[j] * outputs[i][j] for j in range(num_outputs)]
         ) <= lpSum([inputs_weights[j] * inputs[i][j] for j in range(num_inputs)])
@@ -37,7 +40,34 @@ def calculate_efficiency(inputs, outputs, id=0) -> float:
     # Rozwiązanie problemu optymalizacyjnego
     model.solve()
 
-    return round(model.objective.value(), 2)
+    return model
+
+def calculate_efficiency(inputs, outputs, super_efficiency=False):
+    efficiencies = []
+    for i in range(inputs.shape[0]):
+        model = solve_CCR(inputs, outputs, i, super_efficiency)
+        efficiency = round(model.objective.value(), 3)
+        efficiencies.append(efficiency)
+    return efficiencies
+
+
+def cross_efficiency(inputs, outputs, cities):
+    num_examples = inputs.shape[0]
+    num_inputs = inputs.shape[1]
+    num_outputs = outputs.shape[1]
+
+    cross_efficiencies = pd.DataFrame(index=cities, columns=cities)
+
+    for i in range(num_examples):
+        model = solve_CCR(inputs, outputs, i)
+        params = [p.value() for p in model.variables()]
+        output_params, input_params = params[:num_outputs], params[num_outputs:]        
+        for j in range(num_examples):
+            value = round(np.sum(outputs[j] * output_params) / np.sum(inputs[j] * input_params), 3)
+            cross_efficiencies.iloc[i, j] = value
+    
+    cross_efficiencies.loc['CRE_k'] = cross_efficiencies.mean()
+    return cross_efficiencies.T
 
 
 def main():
@@ -48,13 +78,22 @@ def main():
     inputs_np = inputs.to_numpy()
     outputs_np = outputs.to_numpy()
 
-    efficiencies = []
-    for i in range(inputs.shape[0]):
-        efficiency = calculate_efficiency(inputs_np, outputs_np, id=i)
-        efficiencies.append(efficiency)
+    efficiencies = calculate_efficiency(inputs_np, outputs_np)
+    super_efficiencies = calculate_efficiency(inputs_np, outputs_np, super_efficiency=True)
+    cross_efficiencies = cross_efficiency(inputs_np, outputs_np, cities)
     
+    print("Efficiencies")
     for city, e in zip(cities, efficiencies):
         print(f"{city}: {e}")
+
+    print("Super-Efficiencies")
+    for city, e in zip(cities, super_efficiencies):
+        print(f"{city}: {e}")
+
+    print("Cross Efficiencies")
+    print(cross_efficiencies)
+
+    
 
 
 if __name__ == "__main__":
