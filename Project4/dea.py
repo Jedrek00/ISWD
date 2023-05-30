6ad199e3baf60b8fdc627f71de195f92ebec0154
Jedrek00 <jedrek.koscielniak@gmail.com>
2023-05-31 00:44:42 +0200

    Add hcu

--- Project4/dea.py
+++ Project4/dea.py
@@ -44,6 +44,70 @@ def solve_CCR(inputs, outputs, id, super_efficiency=False) -> LpProblem:
     return model
 
 
+def solve_HCU(inputs, outputs, id) -> LpProblem:
+    num_examples = inputs.shape[0]
+    num_inputs = inputs.shape[1]
+    num_outputs = outputs.shape[1]
+
+    # Tworzenie problemu optymalizacyjnego
+    model = LpProblem("EfficiencyCalculation", LpMinimize)
+
+    # Tworzenie zmiennej decyzyjnej dla każdego wejścia oraz wyjścia
+    lambdas = [
+        LpVariable(f"l_{i}", lowBound=0, cat="Continuous") for i in range(num_examples)
+    ]
+
+    theta = LpVariable(f"t", lowBound=0, cat="Continuous")
+
+    # Dodanie ograniczeń
+    for i in range(num_inputs):
+        model += lpSum(
+            [lambdas[j] * inputs[j][i] for j in range(num_examples)]
+        ) <= theta * inputs[id][i]
+
+    for i in range(num_outputs):
+        model += lpSum(
+            [lambdas[j] * outputs[j][i] for j in range(num_examples)]
+        ) >= outputs[id][i]
+
+    # Tworzenie funkcji celu
+    model += theta
+
+    # Rozwiązanie problemu optymalizacyjnego
+    model.solve(PULP_CBC_CMD(msg=0))
+
+    return model
+
+
+def calculate_hcu(inputs, outputs):
+    num_examples = inputs.shape[0]
+    num_inputs = inputs.shape[1]
+    all_hcu_inputs = []
+    all_differences = []
+    for i in range(num_examples):
+        model = solve_HCU(inputs, outputs, i)
+        params = [p.value() for p in model.variables()[:-1]]
+        # params are in lexical order, so l_10 was after l_0 and l_1
+        params[2:] = params[3:] + [params[2]]
+        hcu_inputs = []
+        for n in range(num_inputs):
+            value = round(np.dot(params, inputs[:, n]), 3)
+            hcu_inputs.append(value)
+        all_hcu_inputs.append(hcu_inputs)
+    
+    for i in range(num_examples):
+        diffs = []
+        for n in range(num_inputs):
+            value = round(inputs[i][n] - all_hcu_inputs[i][n], 3)
+            diffs.append(value)
+        all_differences.append(diffs)
+    
+    all_hcu_inputs = pd.DataFrame(all_hcu_inputs)
+    all_differences = pd.DataFrame(all_differences)
+
+    print(pd.concat([all_hcu_inputs, all_differences], axis=1))
+
+
 def calculate_efficiency(inputs, outputs, super_efficiency=False):
     efficiencies = []
     for i in range(inputs.shape[0]):
@@ -103,11 +167,11 @@ def distribution_of_efficiencies(inputs, outputs, samples, bins):
     
     return result
 
+
 def display_ranking(values: list, cities:list):
     sorted_values, sorted_cities = zip(*sorted(zip(values, cities), reverse=True))
     for i,  (value, city) in enumerate(zip(sorted_values, sorted_cities)):
         print(f"{i+1}. {city}: {value:.3f}")
-
 
 
 def main():
@@ -123,6 +187,8 @@ def main():
     outputs_np = outputs.to_numpy()
     samples_np = samples.to_numpy()
 
+    calculate_hcu(inputs_np, outputs_np)
+
     # calculate different types of efficiencies
     efficiencies = calculate_efficiency(inputs_np, outputs_np)
     super_efficiencies = calculate_efficiency(inputs_np, outputs_np, super_efficiency=True)
@@ -130,27 +196,27 @@ def main():
     distribution = distribution_of_efficiencies(inputs_np, outputs_np, samples_np, BINS)
     
     # display results
-    print("Efficiencies")
-    for city, e in zip(cities, efficiencies):
-        print(f"{city}: {e}")
+    # print("Efficiencies")
+    # for city, e in zip(cities, efficiencies):
+    #     print(f"{city}: {e}")
 
-    print("Super-Efficiencies")
-    for city, e in zip(cities, super_efficiencies):
-        print(f"{city}: {e}")
+    # print("Super-Efficiencies")
+    # for city, e in zip(cities, super_efficiencies):
+    #     print(f"{city}: {e}")
 
-    print("Cross Efficiencies")
-    print(cross_efficiencies)
+    # print("Cross Efficiencies")
+    # print(cross_efficiencies)
 
-    print("Distribution Of Efficiencies")
-    print(pd.DataFrame(distribution, index=cities))
+    # print("Distribution Of Efficiencies")
+    # print(pd.DataFrame(distribution, index=cities))
 
-    print("==RANKINGS==")
-    print("Super-Efficiencies")
-    display_ranking(super_efficiencies, cities)
-    print("Mean values from Cross Efficiencies")
-    display_ranking(cross_efficiencies['CRE_k'].to_list(), cities)
-    print("Expected values of Efficiencies")
-    display_ranking(list(distribution[:, BINS]), cities)
+    # print("==RANKINGS==")
+    # print("Super-Efficiencies")
+    # display_ranking(super_efficiencies, cities)
+    # print("Mean values from Cross Efficiencies")
+    # display_ranking(cross_efficiencies['CRE_k'].to_list(), cities)
+    # print("Expected values of Efficiencies")
+    # display_ranking(list(distribution[:, BINS]), cities)
 
     
 if __name__ == "__main__":
